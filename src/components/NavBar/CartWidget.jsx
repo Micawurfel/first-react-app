b5d5c05f0711d9a0a566c9bd4f688b46a7d50700
Micawurfel <micawurfel@gmail.com>
2022-12-14 08:49:52 -0500

    navbar

---
+++ src/components/NavBar/CartWidget.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+const CartWidget = () => {
+    return(
+        <>
+            <a href="#">
+                <FontAwesomeIcon icon="fa-regular fa-cart-shopping" />
+            </a>
+        </>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+const NavBar = () => {
+    return(
+        <>
+            <div>
+                <a href="">
+                    <img src="" alt="" />
+                </a>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default NavBar
